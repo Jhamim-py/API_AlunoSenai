daab1e0d30e384978ef54e7f2349761751af9e28
Jhamim-py <141651527+Jhamim-py@users.noreply.github.com>
2024-05-13 08:45:28 -0300

    Comentando o código

--- mesa/src/index.js
+++ mesa/src/index.js
@@ -1,11 +1,13 @@
 const express =  require("express");
 const { default: mongoose } = require("mongoose");
 const app = express()
+
+//Passando para json
 app.use(express.json())
 const port = 3000;
 mongoose.connect('mongodb+srv://houtarousenki:pYuWQ6LRAXpSgmQL@mesa.wybqozy.mongodb.net/?retryWrites=true&w=majority&appName=mesa')
 
-//Resolver problemas com permições
+//Resolvendo problemas com CORS
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -21,7 +23,7 @@ app.use((req, res, next) => {
   
 
 
-
+//Modelo
 const Mesa = mongoose.model('Mesa',{
     nome: String,
     descricao: String,
@@ -30,19 +32,20 @@ const Mesa = mongoose.model('Mesa',{
 })
 
 
+//Utilizar o método GET utilizando o ID para pegar um dado específico
 app.get("/:id",async(req, res) => {
     const mesa = await Mesa.findById(req.params.id)
     res.send(mesa)
 })
 
-
+//Utilizar o método GET para pegar todos os dados armazenados
 app.get("/",async(req, res) => {
     const mesas = await Mesa.find()
     res.send(mesas)
 })
 
 
-
+//Utilizando o método POST para adicionar um dado
 app.post("/", async (req,res) =>{
     const mesa = new Mesa({
         nome: req.body.nome,
@@ -54,6 +57,7 @@ app.post("/", async (req,res) =>{
    res.send(mesa)
 })
 
+//Conectando no banco de dados,MongoDB, e rodando na porta especificada
 app.listen(port, () =>{
     mongoose.connect('mongodb+srv://houtarousenki:pYuWQ6LRAXpSgmQL@mesa.wybqozy.mongodb.net/?retryWrites=true&w=majority&appName=mesa')
 
@@ -61,12 +65,13 @@ app.listen(port, () =>{
 })
 
 
-
+//Utilizando o método delete para deletar um dado específico urilizando um ID
 app.delete( '/:id', async ( req ,res)=>{
     const mesa = await Mesa.findByIdAndDelete(req.params.id);
     return res.send(mesa);
 });
 
+//Utilizando o método PUT para atualizar um dado específico
 app.put('/:id', async (req, res)=> {
    const mesa = await Mesa.findByIdAndUpdate(req.params.id,{ 
        nome : req.body.nome,
